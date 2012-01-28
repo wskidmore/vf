bbd4302c24e7b1f8a5a0923add7b0486f3fba41d
Wskidmore <wskidmore@gmail.com>
2012-01-28 01:52:46 -0700

    iscroll fix

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@
         var loaded = VF.load();
         if(loaded)
             VF.addResume();
-            
+        VF.initNodeScroll();
         $( $.mobile.initializePage );
     };
     VF.addResume = function(){
@@ -170,6 +170,56 @@
     VF.onOrientationChange = function(evt){
         $('html').removeClass('portrait landscape').addClass(evt.orientation);
     };
+    VF.initNodeScroll = function (elm) {
+		var SafariWindowHeightFix = 34; // XXX:
+		if (elm.data("iscroll-plugin")) {
+			return;
+		}
 
+		// XXX: fix crumbled css in transition changePage 
+		// for jquery mobile 1.0a3 in jquery.mobile.navigation.js changePage
+		//  in loadComplete in removeContainerClasses in .removeClass(pageContainerClasses.join(" "));
+		elm.css({
+			overflow: 'hidden'
+		});
+
+		var barHeight = 0;
+		var $header = elm.find('[data-role="header"]');
+		if ($header.length) {
+			$header.css({
+				"z-index": 1000,
+				padding: 0,
+				width: "100%"
+			});
+			barHeight += $header.height();
+		}
+
+		var $footer = elm.find('[data-role="footer"]');
+		if ($footer.length) {
+			$footer.css({
+				"z-index": 1000,
+				padding: 0,
+				width: "100%"
+			});
+			barHeight += $footer.height();
+		}
+
+		var $wrapper = elm.find('[data-role="content"]');
+		if ($wrapper.length) {
+			$wrapper.css({
+				"z-index": 1
+			});
+	//		$wrapper.height($(window).height() - barHeight);
+		}
+
+		var iscroll = new iScroll($wrapper.get(0), {desktopCompatibility: true});
+		elm.data("iscroll-plugin", iscroll);
+	};
+	VF.setScrolls = function(){
+		$('[data-role="page"][data-iscroll="enable"]').live("pagecreate", function() {
+			console.log('calling');
+			VF.initNodeScroll($(this));
+		});
+	};
 
 })(window.VF = window.VF || {}, document);
